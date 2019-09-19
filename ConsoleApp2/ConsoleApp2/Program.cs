ee071cc270900b671de740f4bd507c21ef6b1c50
2722583179 <2722583179@qq.com>
2019-09-19 14:28:58 +0800

    2.0

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string c1 = "Hello";
+            string c2= " World!";
+            Console.WriteLine(c1+c2);
+        }
+    
+}
