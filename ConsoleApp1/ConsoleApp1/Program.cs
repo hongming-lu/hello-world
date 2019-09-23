23a7de6c12d6139e221668824e85a75e9d7b9937
2722583179 <2722583179@qq.com>
2019-09-23 15:56:09 +0800

    yinyong

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,108 +1,26 @@
 ﻿using System;
-using Wrox.ProCSharp.VenusBank;
-using Worx.ProCsharp.JupiterBank;
-using Wrox.ProCSharp;
 
-namespace Wrox.ProCSharp
+namespace ConsoleApp1
 {
-
-    public interface IBankAccount
+    class Program
     {
-        void PayIn(decimal amount);
-        bool Withdraw(decimal amount);
-        decimal Balance { get; }
-    }
-    class MainnEntryPoint
-    {
-        static void Main()
+         class Vecter
         {
-            IBankAccount venusAccount = new SaverAccount();
-            IBankAccount jupiterAccount = new GoidAccount();
-            venusAccount.PayIn(200);
-            venusAccount.Withdraw(100);
-            Console.WriteLine(venusAccount.ToString());
-            jupiterAccount.PayIn(500);
-            jupiterAccount.Withdraw(600);
-            jupiterAccount.Withdraw(100);
-            Console.WriteLine(jupiterAccount.ToString());
-            /*------*/
+          public  int Value;
         }
-    }
-}
 
-namespace Wrox.ProCSharp.VenusBank
-{
-
-
-    public class SaverAccount : IBankAccount
-    {
-        private decimal balance;
-        public void PayIn(decimal amount)
+        static void Main(string[] args)
         {
-            balance += amount;
-        }
-        public bool Withdraw(decimal amount)
-        {
-            if (balance >= amount)
-            {
-                balance -= amount;
-                return true;
-            }
-            Console.WriteLine("withdrawal attempt failed.");
-            return false;
-        }
-        public decimal Balance
-        {
-            get
-            {
-                return balance;
-            }
-        }
+            Vecter x, y;
+            x = new Vecter ();
+            x.Value=30;
+            y = x;
+            Console.WriteLine(y.Value );
+            Console.WriteLine(x.Value);
+            y.Value  = 50;
 
-        decimal IBankAccount.Balance => throw new NotImplementedException();
+            Console.WriteLine(x.Value);
 
-        public override string ToString()
-        {
-            return string.Format("Venus Bank Saver:Balance=(0.6:C", balance);
         }
-
-    }
-}
-namespace Worx.ProCsharp.JupiterBank
-{
-    public class GoidAccount : IBankAccount
-    {
-
-
-        private decimal balance;
-        public void PayIn(decimal amount)
-        {
-            balance += amount;
-        }
-        public bool Withdraw(decimal amount)
-           {
-            if (balance >= amount)
-            {
-                balance -= amount;
-                return true;
-            }
-            Console.WriteLine("withdrawal attempt failed.");
-            return false;
-        }
-        public decimal Balance
-        {
-            get
-            {
-                return balance;
-            }
-        }
-
-        decimal IBankAccount.Balance => throw new NotImplementedException();
-
-        public override string ToString()
-        {
-            return string.Format("Venus Bank Saver:Balance=(0.6:C", balance);
-        }
-
     }
 }
