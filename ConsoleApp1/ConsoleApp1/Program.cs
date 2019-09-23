348bc5a92519220a427c9d87346e06493eab6e3a
2722583179 <2722583179@qq.com>
2019-09-23 14:04:26 +0800

    1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,84 @@
 ﻿using System;
+using Wrox.ProCSharp.VenusBank;
+using Worx.ProCsharp.JupiterBank;
+using Wrox.ProCSharp;
 
-namespace ConsoleApp1
+namespace Wrox.ProCSharp
 {
-    class Program
+
+    public interface IBankAccount
     {
-        static void Main(string[] args)
+        void PayIn(decimal amount);
+        bool Withdraw(decimal amount);
+        decimal Balance { get; }
+    }
+    class MainnEntryPoint
+    {
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            IBankAccount venusAccount = new SaverAccount();
+            IBankAccount jupiterAccount = new GoidAccount();
+            venusAccount.PayIn(200);
+            venusAccount.Withdraw(100);
+            Console.WriteLine(venusAccount.ToString());
+            jupiterAccount.PayIn(500);
+            jupiterAccount.Withdraw(600);
+            jupiterAccount.Withdraw(100);
+            Console.WriteLine(jupiterAccount.ToString());
+            /*------*/
         }
     }
 }
+
+namespace Wrox.ProCSharp.VenusBank
+{
+
+
+    public class SaverAccount : IBankAccount
+    {
+        private decimal balance;
+        public void PayIn(decimal amount)
+        {
+            balance += amount;
+        }
+        public bool Withdraw(decimal amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            Console.WriteLine("withdrawal attempt failed.");
+            return false;
+        }
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+        }
+
+        decimal IBankAccount.Balance => throw new NotImplementedException();
+
+        public override string ToString()
+        {
+            return string.Format("Venus Bank Saver:Balance=(0.6:C", balance);
+        }
+
+    }
+}
+namespace Worx.ProCsharp.JupiterBank
+{
+    public class GoidAccount : IBankAccount
+    {
+
+
+        private decimal balance;
+        public void PayIn(decimal amount)
+        {
+            balance += amount;
+        }
+        public bool Withdraw(decimal amoun）
+            {
+        }
